4725d0478c0a0e5fae383af57e50613052636419
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-17 20:05:32 +0400

    Update DeclaringVars.java

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,7 +3,10 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
-        int a = b = c = 10;
+        int a;
+        int b;
+        int c;
+        a = b = c = 10;
         System.out.println(c);
     }
 }
